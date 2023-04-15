be7de3d0891cfb29a67f5684faf502da1811101d
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-15 17:31:24 +0300

    Update ITstepDef.py
    done

--- ITstepDef.py
+++ ITstepDef.py
@@ -1,1 +1,13 @@
-
+def vl(q):
+    p=""
+    s=0
+    for i in range(len(q)):
+        if q[i-1]==" " and q[i].isalpha():
+            p+=q[i].capitalize()
+        else:
+            p+=q[i]
+    return p
+q=input()
+q=" "+q
+l=vl(q)
+print(l)
