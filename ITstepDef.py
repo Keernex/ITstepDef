5d4ab58ff704b900f8a7462b54a1cc056d7b21c0
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-15 16:56:10 +0300

    Update ITstepDef.py
    done

--- ITstepDef.py
+++ ITstepDef.py
@@ -1,1 +1,14 @@
-
+def Kll(q):
+    p=0
+    gl=0
+    q=q.lower()
+    for i in q:
+        if q.isalpha():
+            if i in 'aeiou':
+                gl+=1
+            else:
+                p+=1
+    return "golos: "+str(gl),"prigolos: "+str(p)
+q=input()
+l=Kll(q)
+print(*l)
