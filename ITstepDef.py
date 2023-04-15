08eec474a9ba54c3cb4b3f1d0363d610e60564c5
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-15 18:28:28 +0300

    Update ITstepDef.py
    done

--- ITstepDef.py
+++ ITstepDef.py
@@ -1,1 +1,12 @@
-
+def nbkz(q):
+    w=""
+    e=0
+    for i in q:
+        if q.count(i)>e:
+            e = q.count(i)
+            w = i
+    return w
+q=list(map(int,input().split()))
+q.sort()
+k=nbkz(q)
+print(k)
