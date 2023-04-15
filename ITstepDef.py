3617c39d965bc5fd9c35dc9bd770b8dd0914974b
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-15 17:59:11 +0300

    Update ITstepDef.py
    done

--- ITstepDef.py
+++ ITstepDef.py
@@ -1,1 +1,11 @@
-
+def vik(l):
+    return sorted(l, key=lambda x: x[1])
+n=int(input())
+l=[]
+for i in range(n):
+    w,r=map(str,input().split())
+    r=int(r)
+    l.append((w,r))
+k=vik(l)
+for i in k:
+    print(*i)
